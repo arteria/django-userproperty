9cd42775cab89ee10f164ec2899d32470c50ffc4
Walter Renner <renner@arteria.ch>
2015-06-18 12:11:58 +0200

    added support for custom user model

--- userproperty/models.py
+++ userproperty/models.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+
+from compat import user_model_label as USER_MODEL  # NOQA
 
 
 @python_2_unicode_compatible
 class UserProperty(models.Model):
     '''
     '''
-    user = models.ForeignKey(User, related_name='userprop')
+    user = models.ForeignKey(USER_MODEL, verbose_name=_('user'), related_name="user_properties")
     name = models.SlugField('name', max_length=64)
     value = models.CharField(max_length=256)
 

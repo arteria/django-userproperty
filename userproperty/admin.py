f851d2998fb20e35e0e2feb3b4ace3cc06dc2e15
Walter Renner <renner@arteria.ch>
2015-07-21 16:02:09 +0200

    fast admin for UserProperty and GlobalProperty
    fixes #10

--- userproperty/admin.py
+++ userproperty/admin.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
@@ -6,14 +5,15 @@ from userproperty.models import UserProperty, GlobalProperty
 
 
 class UserPropertyAdmin(admin.ModelAdmin):
-    list_display = ('user', 'name',)
-    list_filter = ('user', 'name',)
-
-admin.site.register(UserProperty, UserPropertyAdmin)
+    list_display = ('user', 'name', 'value',)
+    raw_id_fields = ('user',)
+    search_fields = ['name', 'value']
 
 
 class GlobalPropertyAdmin(admin.ModelAdmin):
     list_display = ('name', 'value',)
-    list_filter = ('name', 'value',)
+    search_fields = ['name', 'value']
 
+
+admin.site.register(UserProperty, UserPropertyAdmin)
 admin.site.register(GlobalProperty, GlobalPropertyAdmin)

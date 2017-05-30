7822123bd3e4224c51a6687959f8870c76065e9b
Walter Renner <renner@arteria.ch>
2017-05-30 10:39:54 +0200

    Django1.11 compatible urls

--- userproperty/urls.py
+++ userproperty/urls.py
@@ -1,27 +1,23 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from django.conf.urls import url
 
-from django.conf.urls import patterns, url
+from userproperty import views
 
 
-urlpatterns = patterns('userproperty.views',
+urlpatterns = [
     # user properties
     url(r'^get-uproperty/$',
-        view='get_user_property',
-        name='get_user_property',
-    ),
+        views.get_user_property,
+        name='get_user_property', ),
     url(r'^set-uproperty/$',
-        view='set_user_property',
-        name='set_user_property',
-    ),
+        views.set_user_property,
+        name='set_user_property', ),
 
     # global properties
     url(r'^get-gproperty/$',
-        view='get_global_property',
-        name='get_global_property',
-    ),
+        views.get_global_property,
+        name='get_global_property', ),
     url(r'^set-gproperty/$',
-        view='set_global_property',
-        name='set_global_property',
-    ),
-)
+        views.set_global_property,
+        name='set_global_property', ),
+]

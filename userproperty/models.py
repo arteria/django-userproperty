82abcd01319e325461b9c7fd5a3d08317a9db492
Walter Renner <renner@arteria.ch>
2017-12-04 14:35:59 +0100

    use translated model fields

--- userproperty/models.py
+++ userproperty/models.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
@@ -9,10 +8,10 @@ from compat import user_model_label as USER_MODEL  # NOQA
 
 @python_2_unicode_compatible
 class UserProperty(models.Model):
-    '''
-    '''
+    """
+    """
     user = models.ForeignKey(USER_MODEL, verbose_name=_('user'), related_name="user_properties")
-    name = models.SlugField('name', max_length=64)
+    name = models.SlugField(_('name'), max_length=64)
     value = models.CharField(max_length=256)
 
     class Meta:
@@ -28,9 +27,9 @@ class UserProperty(models.Model):
 
 @python_2_unicode_compatible
 class GlobalProperty(models.Model):
-    '''
-    '''
-    name = models.SlugField('name', max_length=64, unique=True)
+    """
+    """
+    name = models.SlugField(_('name'), max_length=64, unique=True)
     value = models.CharField(max_length=256)
 
     class Meta:
